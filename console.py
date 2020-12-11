d4a8a457dc66ec83156e3995839a162b6c81e4a1
fukiran <macdorphoto@yahoo.com>
2020-12-11 21:59:00 +0000

    repositories done

--- console.py
+++ console.py
@@ -12,9 +12,19 @@ session_repository.delete_all()
 
 member1 = Member('Sylvester', 74)
 member_repository.save(member1)
-
-session1 = Session('Yoga', 'just yoga lesson', True)
+member2 = Member('Arnold', 73)
+member_repository.save(member2)
+session1 = Session('Yoga', 'just yoga lesson', False)
 session_repository.save(session1)
+session2 = Session('Boxing', 'by Ivan Drago', True)
+session_repository.save(session2)
+session3 = Session('Belly Dancing', 'by Zoe Jakes', True)
+session_repository.save(session3)
 
 booking1 = Booking(member1, session1)
 bookings_repository.save(booking1)
+booking2 = Booking(member2, session1)
+bookings_repository.save(booking2)
+
+print(session_repository.select_booked_members(session1.id))
+print(session_repository.show_upcoming())
--- repositories/session_repository.py
+++ repositories/session_repository.py
@@ -1,6 +1,7 @@
 from db.run_sql import run_sql
 
 from models.session import Session
+from models.member import Member
 
 def save(session):
     sql = "INSERT INTO sessions(name, description, upcoming) VALUES (%s, %s, %s) RETURNING id"
@@ -16,3 +17,22 @@ def update(session):
 def delete_all():
     sql = "DELETE FROM sessions"
     run_sql(sql)
+
+def select_booked_members(id):
+    members = []
+    sql = "SELECT members.* FROM members INNER JOIN bookings ON bookings.member_id = members.id WHERE bookings.session_id = %s"
+    values = [id]
+    results = run_sql(sql, values)
+    for result in results:
+        member = Member(result["name"], result["age"])
+        members.append(member)
+    return members
+
+def show_upcoming():
+    upcoming = []
+    sql = "SELECT * FROM sessions WHERE sessions.upcoming = True"
+    results = run_sql(sql)
+    for row in results:
+        session = Session(row['name'], row['description'], row['upcoming'])
+        upcoming.append(session)
+    return upcoming
