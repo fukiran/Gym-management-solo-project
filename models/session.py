4d07b51292082d8fd3fe7a80782f59ae9abb71fa
fukiran <macdorphoto@yahoo.com>
2020-12-11 17:50:14 +0000

    added models

---
+++ models/session.py
@@ -0,0 +1,7 @@
+class Session:
+
+    def __init__(self, name, description, upcoming, id):
+        self.name = name 
+        self.description = description
+        self.upcoming = upcoming
+        self.id = id
